09f8807b300fa873c30ee8b46da67fdd2e483dab
Joseph Gordy <jgordy2424@gmail.com>
2017-11-29 15:29:11 -0500

    commented out some old code until i determine if I need it

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // Greeting for main header
-let today = new Date();
-let hourNow = today.getHours();
-let greeting;
-
-if (hourNow >= 18) {
-  greeting = "Good evening!";
-} else if (hourNow >= 12) {
-  greeting = "Good afternoon!";
-} else if (hourNow >= 0) {
-  greeting = "Good morning!";
-} else {
-  greeting = "Welcome!";
-}
-
-document.getElementById('greet').textContent = greeting;
+// let today = new Date();
+// let hourNow = today.getHours();
+// let greeting;
+//
+// if (hourNow >= 18) {
+//   greeting = "Good evening!";
+// } else if (hourNow >= 12) {
+//   greeting = "Good afternoon!";
+// } else if (hourNow >= 0) {
+//   greeting = "Good morning!";
+// } else {
+//   greeting = "Welcome!";
+// }
+//
+// document.getElementById('greet').textContent = greeting;
 
 
 // For the menu toggle
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+// function myFunction(x) {
+//     x.classList.toggle("change");
+// }
 
 
 
